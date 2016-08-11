0d8dbdc2590bc033adc9dd5cdb474c87c913204e
Billie Thompson <billie@purplebooth.co.uk>
2016-08-11 22:16:17 +0100

    Initial Commit
    Created Swagger API Installer to generate API clients from swagger.

---
+++ README.md
@@ -0,0 +1,122 @@
+# Jane OpenAPI Autogenerate
+
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/PurpleBooth/jane-open-api-autogenerate/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/PurpleBooth/jane-open-api-autogenerate/?branch=master)
+[![Build Status](https://travis-ci.org/PurpleBooth/jane-open-api-autogenerate.svg?branch=master)](https://travis-ci.org/PurpleBooth/jane-open-api-autogenerate)
+[![Dependency Status](https://www.versioneye.com/user/projects/57ad0ad6cb5df20031a64a5c/badge.svg?style=flat-square)](https://www.versioneye.com/user/projects/57ad0ad6cb5df20031a64a5c)
+[![Latest Stable Version](https://poser.pugx.org/purplebooth/jane-open-api-autogenerate/v/stable)](https://packagist.org/packages/purplebooth/jane-open-api-autogenerate)
+[![License](https://poser.pugx.org/purplebooth/jane-open-api-autogenerate/license)](https://packagist.org/packages/purplebooth/jane-open-api-autogenerate)
+
+This project is designed to allow you to add an automatically generated
+swagger as a dependency via a composer dependency.
+
+## Getting Started
+
+### Prerequisities
+
+You'll need to install:
+
+ * PHP (Minimum 5.6)
+
+## Usage
+
+You have a few options, you could add in the repository you keep your
+swagger definition define a composer.json that looks something like
+this.
+
+```json
+{
+    "name": "swagger/petstore",
+    "type": "swagger-api",
+    "extra": {
+        "namespace": "Swagger\\Petstore\\",
+        "schema-file": "http://petstore.swagger.io/v2/swagger.json"
+    },
+    "require": {
+        "purplebooth/jane-open-api-autogenerate": "*@dev"
+    },
+    "autoload": {
+        "psr-0": {
+            "Swagger\\Petstore\\": ".",
+        }
+    }
+}
+```
+
+and then run
+
+```
+$ composer require swagger/petstore
+```
+
+alternatively you could add the package manually in the composer.json
+
+```json
+
+{
+    "require": {
+        "swagger/petstore": "0.1.0"
+    },
+    "repositories": [{
+        "type": "package",
+        "package": {
+            "name": "swagger/petstore",
+            "type": "swagger-api",
+            "extra": {
+                "namespace": "Swagger\\Petstore\\",
+                "schema-file": "http://petstore.swagger.io/v2/swagger.json"
+            },
+            "require": {
+                "purplebooth/jane-open-api-autogenerate": "*@dev"
+            },
+            "autoload": {
+                "psr-0": {
+                    "Swagger\\Petstore\\": "."
+                }
+            }
+        }
+    }]
+}
+
+```
+
+Another feature of this library is the ability to override the swagger
+file from a location defined by an environment variable. To do this you
+simply define an additional key in the package specifies in which
+environment variable.
+
+```json
+"extra": {
+    "namespace": "Swagger\\Petstore\\",
+    "schema-file": "http://petstore.swagger.io/v2/swagger.json",
+    "environment-variable": "PETS_SWAGGER_YAML"
+},
+```
+
+If it's not set it'll fall back to the value defined in the schema-file
+attribute.
+
+### Coding Style
+
+We follow PSR2, and also enforce PHPDocs on all functions. To run the tests for coding style violations
+
+```bash
+vendor/bin/phpcs -p --standard=psr2 src/
+```
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code
+of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions
+available, see the [tags on this repository](https://github.com/purplebooth/jane-open-api-autogenerate/tags).
+
+## Authors
+
+See the list of [contributors](https://github.com/purplebooth/jane-open-api-autogenerate/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ src/SwaggerApiInstaller.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace PurpleBooth\JaneOpenApiAutogenerate;
+
+use Composer\Composer;
+use Composer\Installer\BinaryInstaller;
+use Composer\Installer\LibraryInstaller;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Util\Filesystem;
+use Joli\Jane\OpenApi\JaneOpenApi;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SplFileInfo;
+
+class SwaggerApiInstaller extends LibraryInstaller
+{
+    const PACKAGE_TYPE = 'swagger-api';
+
+    const EXTRA_KEY_NAMESPACE = 'namespace';
+
+    const EXTRA_KEY_SCHEMA_FILE = 'schema-file';
+
+    const EXTRA_KEY_ENVIRONMENT_VARIABLE = 'environment-variable';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(
+        IOInterface $inputOutput,
+        Composer $composer,
+        $type = self::PACKAGE_TYPE,
+        Filesystem $filesystem = null,
+        BinaryInstaller $binaryInstaller = null
+    ) {
+        parent::__construct($inputOutput, $composer, $type, $filesystem, $binaryInstaller);
+    }
+
+
+    /**
+     * { @inheritdoc }
+     */
+    protected function updateCode(PackageInterface $initial, PackageInterface $target)
+    {
+        $this->removeCode($initial);
+        $this->installCode($target);
+    }
+
+    /**
+     * { @inheritdoc }
+     */
+    protected function removeCode(PackageInterface $package)
+    {
+        $initialDownloadPath = $this->getInstallPath($package);
+        $this->removeDirectory($initialDownloadPath);
+    }
+
+    /**
+     * @param string $downloadPath
+     */
+    private function removeDirectory($downloadPath)
+    {
+        if (!(new SplFileInfo($downloadPath))->isDir()) {
+            return;
+        }
+
+        $directory = new RecursiveDirectoryIterator($downloadPath);
+        $iterator  = new RecursiveIteratorIterator(
+            $directory,
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+
+        /** @var SplFileInfo $item */
+        foreach ($iterator as $item) {
+            if ($item->isFile()) {
+                unlink($item->getPathname());
+            }
+
+            if ($item->isDir()) {
+                rmdir($item->getPathname());
+            }
+        }
+
+        rmdir($downloadPath);
+    }
+
+    /**
+     * { @inheritdoc }
+     */
+    protected function installCode(PackageInterface $package)
+    {
+        $downloadPath = $this->getInstallPath($package);
+        $this->generateSwaggerClient($package, $downloadPath);
+    }
+
+    /**
+     * Generate the client for a package
+     *
+     * @param PackageInterface $package
+     * @param string           $downloadPath
+     */
+    protected function generateSwaggerClient(PackageInterface $package, $downloadPath)
+    {
+        $janeOpenApi = JaneOpenApi::build();
+
+        $extra             = $package->getExtra();
+        $namespace         = $extra[ self::EXTRA_KEY_NAMESPACE ];
+        $openApiSchemaFile = $extra[ self::EXTRA_KEY_SCHEMA_FILE ];
+
+        if (isset($extra[ self::EXTRA_KEY_ENVIRONMENT_VARIABLE ])) {
+            $envVariableName = $extra[ self::EXTRA_KEY_ENVIRONMENT_VARIABLE ];
+            $envVariable     = getenv($envVariableName);
+
+            if ($envVariable) {
+                $openApiSchemaFile = $envVariable;
+            }
+        }
+
+
+        $this->io->write(
+            "Generating <info>$namespace</info> from <info>$openApiSchemaFile</info>",
+            true,
+            IOInterface::VERBOSE
+        );
+
+        $this->io->write(
+            "Writing to <info>$downloadPath</info>",
+            true,
+            IOInterface::VERY_VERBOSE
+        );
+
+        $files = $janeOpenApi->generate($openApiSchemaFile, $namespace, $downloadPath);
+        $janeOpenApi->printFiles($files, $downloadPath);
+
+        foreach ($files as $file) {
+            $this->io->write("Generated <info>{$file->getFilename()}</info>", true, IOInterface::DEBUG);
+        }
+
+        $this->io->write(
+            "Generated <info>$namespace</info> from <info>$openApiSchemaFile</info>",
+            true,
+            IOInterface::VERBOSE
+        );
+    }
+}
