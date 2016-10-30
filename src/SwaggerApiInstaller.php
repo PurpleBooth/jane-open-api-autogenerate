dae46c308dc7596a88f7bda71612c2374ba3dfe4
Billie Thompson <github@solace-system.co.uk>
2016-10-30 17:10:14 +0000

    Applied fixes from StyleCI

--- src/SwaggerApiInstaller.php
+++ src/SwaggerApiInstaller.php
@@ -4,8 +4,9 @@
  * Copyright (C) 2016 Billie Thompson
  *
  * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
+ * of the MIT license.  See the LICENSE.md file for details.
  */
+
 
 namespace PurpleBooth\JaneOpenApiAutogenerate;
 
